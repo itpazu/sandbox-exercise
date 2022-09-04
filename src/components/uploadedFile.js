ec2c8b3b309a6e33d36c6bcf9647c27d78b976c0
itpazu <itpazu@gmail.com>
2022-09-04 18:56:41 +0300

    add web router

--- src/components/uploadedFile.js
+++ src/components/uploadedFile.js
@@ -1,21 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Chip from '@mui/material/Chip';
+import Popover from '@mui/material/Popover';
+import Divider from '@mui/material/Divider';
+import Stack from '@mui/material/Stack';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
 
-export default function ClickableAndDeletableChips({ file }) {
-  const handleClick = () => {
-    console.info('You clicked the Chip.');
+const Item = styled(Paper)(() => ({
+  textAlign: 'center',
+  height: 60,
+  lineHeight: '60px',
+  padding: '5px',
+}));
+
+export default function ClickableAndDeletableChips({ file, handleDelete }) {
+  const { name, size, type } = file;
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const handleClick = ({ target }) => {
+    setAnchorEl(target);
   };
 
-  const handleDelete = () => {
-    console.info('You clicked the delete icon.');
+  const handlePopoverOpen = ({ target }) => {
+    setAnchorEl(target);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
   };
 
   return (
-    <Chip
-      label={file}
-      variant='outlined'
-      onClick={handleClick}
-      onDelete={handleDelete}
-    />
+    <>
+      <Chip
+        sx={{ border: 'solid', maxWidth: '250px' }}
+        label={name}
+        variant='outlined'
+        onClick={handleClick}
+        onDelete={handleDelete}
+        onMouseEnter={handlePopoverOpen}
+        onMouseLeave={handlePopoverClose}
+      />
+      <Popover
+        id='mouse-over-popover'
+        sx={{
+          pointerEvents: 'none',
+        }}
+        open={!!anchorEl}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        onClose={handlePopoverClose}
+        disableRestoreFocus
+      >
+        <Stack
+          direction='row'
+          divider={<Divider orientation='vertical' flexItem />}
+          alignItems='center'
+          justifyContent='center'
+        >
+          <Item elevation={2}>size: {size}</Item>
+          <Item elevation={2}>type: {type}</Item>
+        </Stack>
+      </Popover>
+    </>
   );
 }
